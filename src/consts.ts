67536f328b44d1296557a1ab383d1690a3a23722
Ryan Parker <156548614+ryanparkr@users.noreply.github.com>
2024-06-29 20:38:46 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,21 +2,15 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
-}
-
-// Work Page
-export const WORK: Page = {
-  TITLE: "Work",
-  DESCRIPTION: "Places I have worked.",
+  TITLE: "Southeast Crytpo Group",
+  DESCRIPTION: "Southeast Crypto website and blog.",
+  AUTHOR: "SCG",
 }
 
 // Blog Page
 export const BLOG: Page = {
-  TITLE: "Blog",
-  DESCRIPTION: "Writing on topics I am passionate about.",
+  TITLE: "Industry thoughts",
+  DESCRIPTION: "Our thoughts about the crypto industry.",
 }
 
 // Projects Page 
@@ -38,11 +32,7 @@ export const LINKS: Links = [
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
-  { 
-    TEXT: "Blog", 
+    TEXT: "Industry thoughts", 
     HREF: "/blog", 
   },
   { 
@@ -58,18 +48,6 @@ export const SOCIALS: Socials = [
     ICON: "email", 
     TEXT: "markhorn.dev@gmail.com",
     HREF: "mailto:markhorn.dev@gmail.com",
-  },
-  { 
-    NAME: "Github",
-    ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
-  },
-  { 
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
   },
   { 
     NAME: "Twitter",
